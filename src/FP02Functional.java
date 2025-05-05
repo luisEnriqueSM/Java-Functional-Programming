b322867be7b57230856922d778b865e602605198
luis_enriquesm96@hotmail.com <luis_enriquesm96@hotmail.com>
2025-05-05 17:53:29 -0600

    Add Filters to print only even numbers.

--- src/FP02Functional.java
+++ src/FP02Functional.java
@@ -4,7 +4,10 @@ public class FP02Functional {
 
     public static void main(String args[]){
 
-        printAllNumbersInListFunctional(List.of(12,9,13,4,12,15));
+        // printAllNumbersInListFunctional(List.of(12,9,13,4,12,15));
+
+        List<Integer> numbers = List.of(12,9,13,4,6,2,4,12,15);
+        printEvenNumbersInListFunctional(numbers);
     }
 
     private static void printAllNumbersInListFunctional(List<Integer> numbers) {
@@ -12,11 +15,19 @@ public class FP02Functional {
         // What to do?
         // what needs to be done with each value
         // For element i, do call print method
-        numbers.stream().forEach(FP02Functional::print); // Method reference
+        // For each element of this stream do this:
+        numbers.stream().forEach(System.out::println); // Method reference
     }
 
-    private static void print(int number){
-        System.out.println(number);
+    private static boolean isEven(int number){
+        return number % 2 == 0;
+    }
+
+    private static void printEvenNumbersInListFunctional(List<Integer> numbers) {
+        numbers.stream()
+                 // Filter - Only allow even numbers
+                .filter(FP02Functional::isEven)
+                .forEach(System.out::println);
     }
 
 }
